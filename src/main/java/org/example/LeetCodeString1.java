b7396319b37196cf0bb16c79ffd26186b934c8ca
that30sCoder <rahulksingh1092@gmail.com>
2024-05-02 10:27:06 +0530

    Hashmap Method for all substring without repeating character

--- src/main/java/org/example/LeetCodeString1.java
+++ src/main/java/org/example/LeetCodeString1.java
@@ -1,25 +1,28 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LeetCodeString1 {
     public static void main(String[] args) {
-        //find the length of the longest substring without repeating characters.
-        String demoString = "pwwkew";
-        int output = nonRepeating(demoString);
+
+        String demoString = "qiwekiiroc";
+        /*//find the length of the longest substring without repeating characters.*/
+      /*  int output = nonRepeating(demoString);
         System.out.println("The count is " + output);
+
+       */
+        //remove duplicates from string and keep the order same
+        String output = removeDups(demoString);
+        System.out.println("Modified String is" + output);
     }
 
     private static int nonRepeating(String demoString) {
         char[] chars = demoString.toCharArray();
 
         int counter = 0;
-        //Initialized right pointer
+        //Initialized left  pointer
         int k = 0;
-        //Initialized left pointer
-        int l = 0;
+
 
         int maxCounter = 0;
         //Hashmap for the string
@@ -29,24 +32,43 @@ public class LeetCodeString1 {
 
        Map<Character,Integer> countMap = new HashMap<>();
 
-        while (k<demoString.length()) {
-            counter = 0;
-            for (int j = k;j<demoString.length();j++){
 
-                if(countMap.containsKey(chars[j]) && countMap.get(chars[j])>=k && countMap.get(chars[j])<=j){
-                    maxCounter = Math.max(counter,maxCounter);
+
+        //Hashmap Approach
+
+
+
+            for (int j = 0;j<demoString.length();j++){
+
+                if(countMap.containsKey(chars[j]) && countMap.get(chars[j])>=k ){
                     k = countMap.get(chars[j])+1;
-                    countMap.clear();
-                    break;
                 }
-                else {
-                    countMap.put(chars[j], j);
-                    counter++;
-                }
+
+                countMap.put(chars[j], j);
+                maxCounter = Math.max(maxCounter,j-k+1);
+            }
+
+
+
+
+        return maxCounter;
+    }
+
+
+    static String removeDups(String S) {
+        // code here
+        StringBuilder sb = new StringBuilder();
+       // char[] chars = S.toCharArray();
+        int j = 0;
+        Set<Character>stringSet = new HashSet<>();
+        for(int i = 0;i<S.length();i++){
+            if(!stringSet.contains(S.charAt(i))){
+                stringSet.add(S.charAt(i));
+                sb.append(S.charAt(i));
 
             }
 
         }
-        return Math.max(maxCounter,counter);
+        return sb.toString();
     }
 }
