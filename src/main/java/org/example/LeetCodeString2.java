ddfd12ecf5621610036d75b748eadb5b514f26e6
that30sCoder <rahulksingh1092@gmail.com>
2024-05-09 14:19:36 +0530

    Check

--- src/main/java/org/example/LeetCodeString2.java
+++ src/main/java/org/example/LeetCodeString2.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class LeetCodeString2 {
     public static void main(String[] args) {
-        //Longest Palindromic Substring
-        String s = "cbbd";
+        //Longest Palindromic Substring,default result should be one char
+        String s = "ac";
         String response = longestPalindrome(s);
         System.out.println("Longest pallindromic substring is " + response);
 
@@ -14,16 +14,23 @@ public class LeetCodeString2 {
         int i = 0;
         int j = s.length()-1;
 
-        while(i<j){
+        while(i<=j){
             int mid = i+ (j-i)/2 ;
-            if(s.substring(i, mid+1 )
-                    .equals(new StringBuilder(s.substring(mid,j+1)).reverse().toString()))
-                            {
-                return s.substring(i,j+1);
+            if((j-i)%2==0) {
+                if (s.substring(i, mid + 1)
+                        .equals(new StringBuilder(s.substring(mid, j + 1)).reverse().toString())) {
+                    return s.substring(i, j + 1);
+                }
+            }
+            else {
+                if (s.substring(i, mid+1 )
+                        .equals(new StringBuilder(s.substring(mid+1, j+1)).reverse().toString())) {
+                    return s.substring(i, j + 1);
+                }
             }
             i++;
             j--;
         }
-        return "";
+        return s.substring(i);
     }
 }
